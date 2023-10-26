ac7df65facdb0aa53b4f121da72c0130f58ca498
Lucas S <lucas.sorribes@gmail.com>
2023-10-26 21:57:20 +0200

    Finish 1.6-11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -78,10 +78,10 @@ const Statistics = ({data}) => {
 
 const StatisticLine = ({text, value}) => {
   return (
-  <tr>
-    <td>{text}</td>
-    <td>{value}</td>
-  </tr>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
