66b38e53687305336792769d7218ee99331a5c5f
Lucas S <lucas.sorribes@gmail.com>
2023-10-26 21:55:12 +0200

    Finish 1.6-11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -33,32 +33,56 @@ const App = () => {
     const total = totalClicks()
     if (total === 0) return 0
 
-    return `${(100 * good) / totalClicks()} %`
+    return (100 * good) / totalClicks()
   }
 
   return (
     <div>
       <Header text="give feedback"/>
-
       <Button onClick={handleGoodClick} text='good'/>
       <Button onClick={handleNeutralClick} text='neutral'/>
       <Button onClick={handleBadClick} text='bad'/>
 
       <Header text="statistics"/>
-      <ul>
-        <Statistic text='good' clicks={good} />
-        <Statistic text='neutral' clicks={neutral} />
-        <Statistic text='bad' clicks={bad} />
-        <Statistic text='all' clicks={totalClicks()} />
-        <Statistic text='average' clicks={averageScoreClicks()} />
-        <Statistic text='positives' clicks={positivePercentageClicks()} />
-      </ul>
+      <Statistics data={{
+          good: good,
+          neutral: neutral,
+          bad: bad,
+          all: totalClicks(),
+          average: averageScoreClicks(),
+          positivePercentage: positivePercentageClicks(),
+        }}
+      />
     </div>
   )
 }
 
 const Header = ({text}) => <h1>{text}</h1>
+
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-const Statistic = ({text, clicks}) => <li>{text}: {clicks}</li>
+
+const Statistics = ({data}) => {
+  if (data.all === 0) return <p>No feedback given!</p>
+  
+  return (
+    <table>
+      <tbody>
+        {Object.keys(data).map((k, i) => {
+          const value = /percentage/i.test(k) ? `${data[k]} %` : data[k]
+          return <StatisticLine key={i} text={k} value={value}/>
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+const StatisticLine = ({text, value}) => {
+  return (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+  )
+}
 
 export default App
