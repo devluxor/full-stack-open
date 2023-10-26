04ea1510c126ff89877482c8363775c3f72ea3cd
Lucas S <lucas.sorribes@gmail.com>
2023-10-26 15:50:15 +0200

    Remove comments in exercises for part1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,11 +1,3 @@
-// hree new components: Header, Content, and Total
-// All data still resides in the App component, 
-// which passes the necessary data to each component using props. 
-
-// Header takes care of rendering the name of the course, 
-// Content renders the parts and their number of exercises
-// Total renders the total number of exercises.
-
 const Header = ({name}) => {
   return (
     <>
