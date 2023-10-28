4d0c9a03b163477d6e7f4cee974c3e3fa30e2d1d
Lucas S <lucas.sorribes@gmail.com>
2023-10-29 01:41:42 +0200

    Finish 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,10 +5,20 @@ import AddContactForm from './components/Forms'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas', number: '123' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [searchString, setSearchString] = useState('')
+
+  const contactsToShow = showAll 
+    ? persons 
+    : persons.filter(person => (new RegExp(`${searchString}`, 'iu')).test(person.name))
 
   const addNewContact = (event) => {
     event.preventDefault()
@@ -36,6 +46,13 @@ const App = () => {
   return (
     <>
       <Header text='Phonebook' />
+      <SearchInput
+        setSearchString={setSearchString}     
+        setShowAll={setShowAll}     
+      />
+
+      <Header text='Add a new contact' />
+
       <AddContactForm  
         newContact={addNewContact} 
         setNewName={setNewName} 
@@ -44,13 +61,28 @@ const App = () => {
         newNumber={newNumber}
         />
       <Header text='Numbers' />
-      <ContactList contacts={persons} />
+      <ContactList contacts={contactsToShow} />
     </>
   )
 }
 
 const Header = ({text}) => {
   return <h2>{text}</h2>
+}
+
+const SearchInput = ({setShowAll, setSearchString}) => {
+  const handleSearch = (e) => {
+    const searchString = e.target.value
+    setSearchString(searchString)
+    setShowAll(searchString.trim() === '') 
+  }
+
+  return (
+    <input
+      placeholder='Search by name...'
+      onChange={handleSearch}
+    ></input>
+  )
 }
 
 export default App
