fb88e0fdf2d9921a475b260c8183eb7a26bffed9
Lucas S <lucas.sorribes@gmail.com>
2023-11-09 22:13:04 +0100

    Add ex. 6.14-15

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -2,13 +2,16 @@ import { useDispatch } from 'react-redux'
 import { addAnecdote } from '../reducers/anecdoteReducer'
 import { displayNotification, removeNotification } from '../reducers/notificationReducer'
 
+import anecdoteService from '../services/anecdotes'
+
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const newNote = event => {
+  const newAnecdote = async event => {
     event.preventDefault()
     const content = event.target.anecdote.value
-    dispatch(addAnecdote(content))
+    const createdAnecdote = await anecdoteService.createNew(content)
+    dispatch(addAnecdote(createdAnecdote))
     dispatch(displayNotification(`You created ${content}!`))
     setTimeout(() => dispatch(removeNotification()), 4000)
   }
@@ -16,7 +19,7 @@ const AnecdoteForm = () => {
   return (
     <>
       <h2>create new</h2>
-      <form onSubmit={e => newNote(e)}>
+      <form onSubmit={e => newAnecdote(e)}>
         <div>
           <input type='text' name='anecdote'/>
         </div>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -2,26 +2,26 @@
 
 import { createSlice } from '@reduxjs/toolkit'
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+// const anecdotesAtStart = [
+//   'If it hurts, do it more often',
+//   'Adding manpower to a late software project makes it later!',
+//   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+//   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+//   'Premature optimization is the root of all evil.',
+//   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+// ]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
+// const asObject = (anecdote) => {
+//   return {
+//     content: anecdote,
+//     id: getId(),
+//     votes: 0
+//   }
+// }
 
-const initialState = anecdotesAtStart.map(asObject)
+// const initialState = anecdotesAtStart.map(asObject)
 
 // const reducer = (state = initialState, action) => {
 //   switch(action.type) {
@@ -68,14 +68,10 @@ const sortByVotes = anecdotes => {
 
 const anecdotesSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers: {
     addAnecdote(state, action) {
-      const content = action.payload
-      state.push({
-        content,
-        id: getId()
-      })
+      state.push(action.payload)
     },
 
     vote(state, action) {
@@ -87,10 +83,18 @@ const anecdotesSlice = createSlice({
       }
       const updatedAnecdotes = state.map(a => a.id === id ? updatedAnecdote : a)
       return sortByVotes(updatedAnecdotes)
+    },
+
+    appendAnecdote(state, action) {
+      state.push(action.payload)
+    },
+
+    setAnecdotes(state, action) {
+      return action.payload
     }
   }
 })
 
-export const { addAnecdote, vote } = anecdotesSlice.actions
+export const { addAnecdote, vote, appendAnecdote, setAnecdotes } = anecdotesSlice.actions
 
 export default anecdotesSlice.reducer
