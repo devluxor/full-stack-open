263ec96eaead3f454de88bdaa70f0b0e39a8ca0a
Lucas S <lucas.sorribes@gmail.com>
2023-11-14 17:42:54 +0100

    Refactor router syntax in controllers/users

--- part7/backend-bloglist/controllers/users.js
+++ part7/backend-bloglist/controllers/users.js
@@ -4,44 +4,45 @@ const User = require('../models/user')
 
 const MIN_PASSWORD_LENGTH = 3
 
-usersRouter.get('/', async (request, response) => {
-  const users = await User.find({}).populate('blogs', {title: 1, author: 1, likes: 1})
-  // the argument given to the populate method defines that the ids referencing
-  // blog objects in the blogs field of the user document will be replaced
-  // by the referenced blog document (pseudo join queries)
+usersRouter
+  .route('/')
+  .get(async (request, response) => {
+    const users = await User.find({}).populate('blogs', {title: 1, author: 1, likes: 1})
+    // the argument given to the populate method defines that the ids referencing
+    // blog objects in the blogs field of the user document will be replaced
+    // by the referenced blog document (pseudo join queries)
 
-  // The functionality of the populate method of Mongoose is based on the 
-  // fact that we have defined "types" to the references in the Mongoose 
-  // schema with the ref option:
+    // The functionality of the populate method of Mongoose is based on the 
+    // fact that we have defined "types" to the references in the Mongoose 
+    // schema with the ref option:
 
-  response.json(users)
-})
+    response.json(users)
+  })
+  .post(async (request, response) => {
+    const { username, name, password } = request.body
+  
+    if (!password || password.length < MIN_PASSWORD_LENGTH ){     
+      return response.status(400).json({error: 'invalid password'})
+    }
+    
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+  
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    })
+  
+    const savedUser = await user.save()
+  
+    response.status(201).json(savedUser)
+  })
 
 usersRouter.get('/:id', async (request, response) => {
   const id = request.params.id
   const user = await User.findById(id).populate('blogs', {title: 1, author: 1, likes: 1})
   response.json(user)
-})
-
-usersRouter.post('/', async (request, response) => {
-  const { username, name, password } = request.body
-
-  if (!password || password.length < MIN_PASSWORD_LENGTH ){     
-    return response.status(400).json({error: 'invalid password'})
-  }
-  
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
-
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  })
-
-  const savedUser = await user.save()
-
-  response.status(201).json(savedUser)
 })
 
 module.exports = usersRouter
